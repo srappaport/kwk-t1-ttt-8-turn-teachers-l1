afb84176cfee0e65dc974142be363a569fc5d6fe
srappaport <sarah.rappaport@greendot.org>
2018-05-05 12:42:40 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -45,11 +45,10 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index=input_to_index(input)
-  puts index
   if valid_move?(board, index) == true
     move(board, index, value="X")
     display_board(board)
   else
-    puts "Please enter 1-9"
+    puts "Please enter 1-9:"
   end
 end
